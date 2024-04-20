cd9ac80c942d8c10becb84e44d4d7d43a22ce0ef
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-04-20 15:03:02 +0100

    Add fetch data do ProductsPage

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,5 @@
+import { css } from "@emotion/css";
+
+function ProductCard() {}
+
+export default ProductCard;
--- src/components/pages/ProductsPage.jsx
+++ src/components/pages/ProductsPage.jsx
@@ -1,4 +1,32 @@
+import { css } from "@emotion/css";
+import { useEffect } from "react";
+import { useState } from "react";
+
 function ProductsPage() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/category/electronics", {
+      mode: "cors",
+    })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("Server error");
+        }
+        return response.json();
+      })
+      .then((response) => setProducts(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (error) return <p>A network error was encountered</p>;
+  if (loading) return <p>Loading...</p>;
+
+  console.log(products);
+
   return <div>Products</div>;
 }
 
