4f4ddc9853c426d172d7df4b3ad2f47cc4a6ca57
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-04-26 16:23:47 +0100

    Add footer and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,32 @@
 import { css } from "@emotion/css";
+import { IonIcon } from "@ionic/react";
+import { logoGithub } from "ionicons/icons";
 
 function Footer() {
-  return <div className={footer}>Placeholder</div>;
+  return (
+    <div className={footer}>
+      Copyright © 2024 miguelgbandeira
+      <a
+        href="https://github.com/miguelgbandeira/cv-builder-react"
+        target="_blank"
+      >
+        <IonIcon className={githubIcon} icon={logoGithub} id="github-icon" />
+      </a>
+    </div>
+  );
 }
 
 const footer = css`
-  background-color: #aaa;
+  background-color: var(--main-purple);
+  color: white;
   text-align: center;
   padding: 1em;
   margin-top: 2em;
+`;
+
+const githubIcon = css`
+  color: white;
+  margin-left: 0.5em;
 `;
 
 export default Footer;
