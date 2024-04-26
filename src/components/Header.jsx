ec6df8230a1ddcd2578e3d65e750b5d97536ee89
Miguel Bandeira <61510901+miguelgbandeira@users.noreply.github.com>
2024-04-26 16:30:42 +0100

    Add navbar navigation animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,36 @@
 import { css } from "@emotion/css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 
 function Header({ cartCount }) {
+  const location = useLocation();
+  const isActive = (path) => location.pathname === path;
   return (
     <div className={header}>
       <Link to="/" style={{ textDecoration: "none" }}>
         <h1 className={navTitle}>FreshFinds</h1>
       </Link>
       <div className={nav}>
-        <Link className={linkStyle} to="/">
+        <Link
+          className={isActive("/") ? `${linkStyle} ${activeStyle}` : linkStyle}
+          to="/"
+        >
           home
         </Link>
-        <Link className={linkStyle} to="products">
+        <Link
+          className={
+            isActive("/products") ? `${linkStyle} ${activeStyle}` : linkStyle
+          }
+          to="/products"
+        >
           products
         </Link>
-        <Link className={linkStyle} to="checkout">
+        <Link
+          className={
+            isActive("/checkout") ? `${linkStyle} ${activeStyle}` : linkStyle
+          }
+          to="/checkout"
+        >
           cart {cartCount > 0 ? "(" + cartCount + ")" : ""}
         </Link>
       </div>
@@ -44,6 +59,12 @@ const linkStyle = css`
   margin: 1em;
   text-decoration: none;
   color: black;
+`;
+
+const activeStyle = css`
+  text-decoration: underline;
+  text-decoration-color: var(--light-purple);
+  text-decoration-thickness: 5px;
 `;
 
 Header.propTypes = {
